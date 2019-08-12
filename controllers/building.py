eeee48894311bd21e07cda658c384ec4b8b43bd3
cmec-seanwilson <seanwilson@mec.science>
2019-08-12 13:42:05 -0700

    add admin page

--- controllers/building.py
+++ controllers/building.py
@@ -1,8 +1,14 @@
 from webapp2 import RequestHandler
 from lib.jinja import jinja_env
 
+from models.building import Building
+
 class BuildingController(RequestHandler):
     def get(self):
+        buildings = Building.query().fetch()
         # fetch buildings from database
+        template_ctx = {
+            "buildings": buildings
+        }
         buildings_template = jinja_env.get_template('templates/index.html')
-        self.response.write(buildings_template.render())
+        self.response.write(buildings_template.render(template_ctx))
