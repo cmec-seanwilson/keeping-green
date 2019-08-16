dbb91ae2917527ce0b66053be29e14fe0fd5dbb0
Sean <hello@imsean.me>
2019-08-15 23:56:00 -0400

    refine directions

--- models/seeder.py
+++ models/seeder.py
@@ -12,21 +12,21 @@ def seed():
     dir_b_lib = Direction(step = 1, message = '<strong>Heads up:</strong> No food or drink is allowed in the library').put()
     dir_b_lib_sub = Direction(step = 1, message = 'Towards the elevator opposite of the <strong>Printing Room</strong> is a trash can').put()
     dir_b_lib_main = Direction(step = 1, message = 'Under the stairs by the <strong>Printing Room</strong> is recycling and trash cans').put()
-    dir_b_lib_2 = Direction( step = 1, message = 'By the <strong>glass entrance</strong>, in the north-west corner are bins').put()
-    dir_b_main = Direction( step = 1, message = 'Inside <strong>Starbucks</strong> and under the steps are bins').put()
-    dir_b_main_2 = Direction( step = 2, message = 'If you\'re in the middle of the main floor there are bins at the entrance or <strong>Auditorium</strong>').put()
-    dir_b_2_1 = Direction( step = 1, message = 'Adjacent to the <strong>Language Lab</strong> are bins').put()
-    dir_b_2_2 = Direction(step = 2, message = 'Between the stairs leading down and the entrance of the <strong>Computer Lab</strong>').put()
+    dir_b_lib_2 = Direction(step = 1, message = 'By the <strong>glass entrance</strong>, in the north-west corner are bins').put()
+    dir_b_main = Direction(step = 1, message = 'Inside <strong>Starbucks</strong> and under the steps are bins').put()
+    dir_b_main_2 = Direction(step = 2, message = 'If you\'re in the middle of the main floor there are bins at the entrance or <strong>Auditorium</strong>').put()
+    dir_b_2_1 = Direction(step = 1, message = 'Adjacent to the <strong>Language Lab</strong> are bins').put()
+    dir_b_2_2 = Direction(step = 1, message = 'There is a bin between the stairs leading down near the entrance of the <strong>Computer Lab</strong>').put()
     dir_ab1_main_front = Direction(step = 1, message = 'There are recycling and trash bins behind the <strong>Security Desk</strog> under the steps leading to the <strong>Cafeteria</strong>').put()
-    dir_ab1_main_middle = Direction(step = 2, message = 'If you\'re in the middle of the main floor, walk towards the entrance or walk towards the <strong>Auditorium</strong>').put()
-    dir_ab1_sub_1_1 = Direction(step=1, message='If you\'re facing the stairs, between the <strong>Public Safety</strong> room and the stairs leading to the main floor are bins on your left or right if you\'re facing the <strong>Public Safety</strong> room').put()
-    dir_ab1_sub_1_2 = Direction(step=2, message='Adjacent to class <strong>C09</strong>').put()
-    dir_ab1_2_1 = Direction(step=1, message='Near the <strong>Main Elevator</strong>, there are bins in the cafeteria').put()
+    dir_ab1_main_middle = Direction(step = 1, message = 'If you\'re in the middle of the main floor, walk towards the entrance or walk towards the <strong>Auditorium</strong>').put()
+    dir_ab1_sub_1_1 = Direction(step = 1, message = 'If you\'re facing the stairs, between the <strong>Public Safety</strong> room and the stairs leading to the main floor are bins on your left or right if you\'re facing the <strong>Public Safety</strong> room').put()
+    dir_ab1_sub_2_1 = Direction(step = 1, message = 'There is a bin adjacent to class <strong>C09</strong>').put()
+    dir_ab1_2_1 = Direction(step=1, message = 'Near the <strong>Main Elevator</strong>, there are bins in the cafeteria').put()
     dir_ab1_2_2 = Direction(step = 1, message = 'There is bin walking opposite from the cafeteria under the <strong>Nurse</strong> sign').put()
-    dir_ab1_2_3= Direction(step=1, message='There are multiple bins in a line separate by a small distance from each other if you\'re walking from the <strong>Elevator</strong> to the <strong>West</strong>').put()
-    dir_ab1_3= Direction(step=1, message='There are multiple bins in a line separate by a small distance from each other if you\'re walking from the <strong>Elevator</strong> to the <strong>West</strong>').put()
-    dir_ab1_4 = Direction(step=1, message='There are multiple bins in a line separate by a small distance from each other if you\'re walking from the <strong>Elevator</strong> to the <strong>West</strong>').put()
-    dir_ab1_5= Direction(step=1, message='There are multiple bins in a line separate by a small distance from each other if you\'re walking from the <strong>Elevator</strong> to the <strong>West</strong>').put()
+    dir_ab1_2_3= Direction(step= 1, message = 'There are multiple bins in a line separate by a small distance from each other if you\'re walking from the <strong>Elevator</strong> to the <strong>West</strong>').put()
+    dir_ab1_3= Direction(step= 1, message = 'There are multiple bins in a line separate by a small distance from each other if you\'re walking from the <strong>Elevator</strong> to the <strong>West</strong>').put()
+    dir_ab1_4 = Direction(step = 1, message = 'There are multiple bins in a line separate by a small distance from each other if you\'re walking from the <strong>Elevator</strong> to the <strong>West</strong>').put()
+    dir_ab1_5= Direction(step = 1, message = 'There are multiple bins in a line separate by a small distance from each other if you\'re walking from the <strong>Elevator</strong> to the <strong>West</strong>').put()
 
     ##
     # CREATE BINS
@@ -38,7 +38,8 @@ def seed():
     bin_b_2 = Bin(directions = [dir_b_2_1, dir_b_2_2]).put()
     bin_ab1_main_front = Bin(directions = [dir_ab1_main_front]).put()
     bin_ab1_main_middle = Bin(directions = [dir_ab1_main_middle]).put()
-    bin_ab1_sub_1 = Bin(directions = [dir_ab1_sub_1_1, dir_ab1_sub_1_2]).put()
+    bin_ab1_sub_1 = Bin(directions = [dir_ab1_sub_1_1]).put()
+    bin_ab1_sub_2 = Bin(directions = [dir_ab1_sub_2_1])
     bin_ab1_2 = Bin(directions = [dir_ab1_2_1]).put()
     bin_ab1_2_2 = Bin(directions = [dir_ab1_2_2]).put()
     bin_ab1_2_3 = Bin(directions = [dir_ab1_2_3]).put()
@@ -56,7 +57,8 @@ def seed():
     # CREATE FLOORS
     ##
     floor_ab1_sub = Floor(name = 'Sub', bins = [
-        bin_ab1_sub_1
+        bin_ab1_sub_1_1,
+        bin_ab1_sub_2_1
     ]).put()
     floor_ab1_main = Floor(name = 'Main', bins = [
         bin_ab1_main_front,
